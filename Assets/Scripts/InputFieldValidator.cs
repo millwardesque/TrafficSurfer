cd990af54428c6a0a8ea888ba136a92c59129bcb
Christopher Millward <christopher.millward@gmail.com>
2015-11-26 12:54:40 -0500

    Updated to Unity 5.2.3 and added minimum-length requirement for high-score name

---
+++ Assets/Scripts/InputFieldValidator.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class InputFieldValidator : MonoBehaviour {
+	public Button submitButton;
+	
+	public void OnValueChangeCheck(string content) {
+		// Disable the submit button if the input field is empty.
+		if (content == "") {
+			submitButton.interactable = false;
+		}
+		else {
+			submitButton.interactable = true;
+		}
+	}
+}
