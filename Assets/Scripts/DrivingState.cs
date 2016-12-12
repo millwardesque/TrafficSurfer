82cb9c51de628592ed3547251ba84e4badd13584
Christopher Millward <christopher.millward@gmail.com>
2016-12-12 13:13:32 -0500

    Updated turns to use arc-length for calculating turn distance.

--- Assets/Scripts/DrivingState.cs
+++ Assets/Scripts/DrivingState.cs
@@ -104,7 +104,7 @@ public class DrivingStateTurning : DrivingState {
 	float m_turnSpeed = 0f;
 	float m_totalTurnAngle = 0f;
 	float m_degreesPerSecond = 0f;
-	float m_turnLength = 1f;
+	float m_turnLength = 2f;
 
 	public override void Enter(CarController car) {
 		Vector2 turnVector = car.TurnDestination - (Vector2)car.transform.position;
@@ -114,19 +114,28 @@ public class DrivingStateTurning : DrivingState {
 		if (cross.z < 0f) {
 			m_totalTurnAngle *= -1f;
 		}
+
+		// Locks all turns at 0, 90, or -90
+		// @TODO Replace this with some proper trig.
+		if (m_totalTurnAngle < -1f) {
+			m_totalTurnAngle = -90f;
+		} else if (m_totalTurnAngle > 1f) {
+			m_totalTurnAngle = 90f;
+		} else {
+			m_totalTurnAngle = 0f;
+		}
+
 		m_degreesPerSecond = m_totalTurnAngle / m_turnLength;
-		turnVector /= m_turnLength;
 
-		// @TODO Switch to using arclength for turn vector calculations?
-		m_turnSpeed = turnVector.magnitude / 2f;
+		float turnRadius = 2f;	// Approximate radius of the turn.  Taken from the top-corner of the intersection tile (which is 2x2 units)
+		float arcLength = turnRadius * 2f * Mathf.PI * m_totalTurnAngle / 360f; 
+		m_turnSpeed = arcLength / m_turnLength;
 
-		Debug.Log (string.Format ("Starting turn in direction {0} ({1} total degrees) at {2} degrees / second", turnDirection, m_totalTurnAngle, m_degreesPerSecond));
+		Debug.Log (string.Format ("Starting turn in direction {0} ({1} total degrees) at {2} degrees / second (arclength {3} vs. {4})", turnDirection, m_totalTurnAngle, m_degreesPerSecond, arcLength, turnVector.magnitude));
 	}
 
 	public override void Update(CarController car) {
 		car.CheckForOtherCars();
-
-		Debug.Log ("Rotating by " + m_degreesPerSecond * Time.deltaTime);
 		car.RotateBy(m_degreesPerSecond * Time.deltaTime);
 
 		Vector2 distance = car.transform.right * m_turnSpeed * Time.deltaTime;
@@ -136,6 +145,7 @@ public class DrivingStateTurning : DrivingState {
 	}
 	
 	public override void Exit(CarController car) {
+		car.CurrentSpeed = m_turnSpeed;
 		car.turnIndicator.DoneTurn();
 	}
 }
